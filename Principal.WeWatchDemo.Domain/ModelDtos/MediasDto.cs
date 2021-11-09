fad319aa4ee2ec67656eb9b1c6c169236f521a1c
Peter Bartoš <peter.bartos@principal.cz>
2021-11-10 00:20:58 +0100

    refactor

--- Principal.WeWatchDemo.Domain/ModelDtos/MediasDto.cs
+++ Principal.WeWatchDemo.Domain/ModelDtos/MediasDto.cs
@@ -3,10 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
-
 namespace Principal.WeWatchDemo.Domain.ModelDtos
 {
     public class MediasDto : BaseResponseModel
--- Principal.WeWatchDemo.Domain/ModelDtos/ReportsDto.cs
+++ Principal.WeWatchDemo.Domain/ModelDtos/ReportsDto.cs
@@ -2,10 +2,6 @@
 using System;
 using System.Collections.Generic;
 
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
-
 namespace Principal.WeWatchDemo.Domain.Models
 {
     public partial class ReportsDto: BaseResponseModel
