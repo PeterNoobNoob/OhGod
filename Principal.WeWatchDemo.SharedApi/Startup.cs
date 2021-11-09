c1be280966201ad81e2eb01625e4a5bc03c33ce6
Peter Bartoš <peter.bartos@principal.cz>
2021-11-10 00:10:05 +0100

    finished configuring swagger

--- Principal.WeWatchDemo.SharedApi/Startup.cs
+++ Principal.WeWatchDemo.SharedApi/Startup.cs
@@ -36,6 +36,11 @@ namespace Principal.WeWatchDemo.SharedApi
 
             services.AddDbContext<WeWatchDemoDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("WeWatchDemoConStrings")));
+
+            services.AddSwaggerGen(gen =>
+            {
+                gen.SwaggerDoc("v1.0", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "WeWatch Demo API", Version = "v1.0" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +65,11 @@ namespace Principal.WeWatchDemo.SharedApi
             });
 
             app.UseSwagger();
+
+            app.UseSwaggerUI(ui =>
+            {
+                ui.SwaggerEndpoint("/swagger/v1.0/swagger.json", "WeWatch Demo API Endpoint");
+            });
         }
     }
 }
